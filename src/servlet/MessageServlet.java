9b4e5a977164999938c137e3f9e98b93a75fe43a
Boli Tao <tblsyx@outlook.com>
2019-01-01 21:44:18 +0800

    Update message servcice

---
+++ src/servlet/MessageServlet.java
@@ -0,0 +1,38 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Boli Tao
+ * @date 2019/1/1
+ */
+public class MessageServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/json; charset=UTF-8");
+
+        String anyNewMessage = req.getParameter("anyNewMessage");
+        String newMessage = req.getParameter("newMessage");
+
+        String sendId = req.getParameter("sendId");
+        String receiveId = req.getParameter("receiveId");
+        String messageStatus = req.getParameter("messageStatus");
+        String messageContent = req.getParameter("messageContent");
+        String time = req.getParameter("time");
+
+        System.out.println(sendId + "; " + receiveId + "; " + time + "; " + messageContent + "; " + messageStatus);
+
+        String addMessageSQL = "";
+        String receiveMessage;
+        String responseMessageService;
+
+        if (anyNewMessage == "anyNewMessage") {
+
+        }
+
+    }
+}
